6cd567ebf4e00162c26e63312c44d2ac95277c29
Niklas Broberg <d00nibro@chalmers.se>
2009-06-20 12:43:24 +0000

    Updates to work with latest version of haskell-src-exts

--- src/Trhsx.hs
+++ src/Trhsx.hs
@@ -52,7 +52,7 @@ process fp fc = prettyPrintWithMode (defaultMode {linePragmas=True}) $
                  transform $ checkParse $ parse fp fc
 
 parse :: String -> String -> ParseResult Module
-parse fn fc = parseModuleWithMode (ParseMode fn allExtensions baseFixities) fcuc
+parse fn fc = parseModuleWithMode (ParseMode fn allExtensions False baseFixities) fcuc
   where fcuc= unlines $ filter (not . isPrefixOf "#") $ lines fc
 
 usageString :: String
